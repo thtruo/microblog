0eb5268cdc94ab71f067c1a99ca5676602784022
Thomas Truongchau <ttruongchau@gmail.com>
2014-02-07 16:38:05 -0500

    setup db and migrations

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object('config') # tell Flask to read the config file and use it
+db = SQLAlchemy(app) # initialize database
 
-from app import views
+from app import views, models
+
--- config.py
+++ config.py
@@ -1,3 +1,9 @@
+import os
+
+# Database configuration Flask-SQLAlchemy SQLite
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Flask-WTF web forms configuration
 CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 OPENID_PROVIDERS = [
@@ -6,3 +12,8 @@ OPENID_PROVIDERS = [
     { 'name': 'AOL', 'url': 'http://openid.aol.com/<username>' },
     { 'name': 'Flickr', 'url': 'http://www.flickr.com/<username>' },
     { 'name': 'MyOpenID', 'url': 'https://www.myopenid.com' }]
+
+# required by the Flask-SQLAlchemy extension
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db') 
+# folder where we will store the SQLAlchemy-migrate data files
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
